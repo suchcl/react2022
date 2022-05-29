fceec89f431b798180cf5553767d3ebe73d70fa9
suchcl <ya5969@163.com>
2022-05-29 22:51:30 +0800

    jsx只能返回单个根元素

--- react1/src/pages/Login.jsx
+++ react1/src/pages/Login.jsx
@@ -1,13 +1,23 @@
 const Login = () => {
     return (
-        <div className="login">
-            <form>
-                <label>用户名:</label><input type="text" placeholder="请输入用户名" /><br />
-                <label>密码:</label><input type="password" placeholder="请输入密码" /><br />
-                <button type="submit">登录</button>
-                <button type="reset">取消</button>
-            </form>
-        </div>
+        <>
+            <div className="login">
+                <form>
+                    <label>用户名:</label><input type="text" placeholder="请输入用户名" /><br />
+                    <label>密码:</label><input type="password" placeholder="请输入密码" /><br />
+                    <button type="submit">登录</button>
+                    <button type="reset">取消</button>
+                </form>
+            </div>
+            <div className="register">
+                <form>
+                    <label>用户名:</label><input type="text" placeholder="请输入用户名" /><br />
+                    <label>密码:</label><input type="password" placeholder="请输入密码" /><br />
+                    <button type="submit">注册</button>
+                    <button type="reset">重置</button>
+                </form>
+            </div>
+        </>
     );
 }
 
