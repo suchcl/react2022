12e2072b5dafdc18e74f9379104ecc5164bae720
suchcl <ya5969@163.com>
2022-05-29 22:46:05 +0800

    html转为jsx

---
+++ react1/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+const Login = () => {
+    return (
+        <div className="login">
+            <form>
+                <label>用户名:</label><input type="text" placeholder="请输入用户名" /><br />
+                <label>密码:</label><input type="password" placeholder="请输入密码" /><br />
+                <button type="submit">登录</button>
+                <button type="reset">取消</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
