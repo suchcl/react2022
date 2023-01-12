8f4cbb5647e9c34bef82713518636c89073bbba4
suchcl <ya5969@163.com>
2023-01-12 14:15:24 +0800

    NavLink组件 高亮

--- reactrouter6/src/components/Header/index.tsx
+++ reactrouter6/src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import styles from "./index.module.less";
 interface PageProps {
 
@@ -13,13 +13,23 @@ const Header: FC<PageProps> = (props) => {
                     <Link to="/home">首页</Link>
                 </li>
                 <li>
-                    <Link to="/newsList">列表</Link>
+                    <Link to={{
+                        pathname: '/newsList',
+                        search: '?sort=date',
+                        hash: '#hash'
+                    }}>列表</Link>
                 </li>
                 <li>
-                    <Link to="/docs">帮助</Link>
+                    <NavLink
+                        to="/docs"
+                        className={({ isActive }) => isActive ? `${styles['link']} ${styles['nav-active']}` : `${styles['link']}`}
+                    >帮助</NavLink>
                 </li>
                 <li>
-                    <Link to="/feedback">反馈</Link>
+                    <NavLink
+                        to="/feedback"
+                        style={({isActive}) => isActive ? ({color: '#6BE61A', fontSize: '16px'}) : ({})}
+                    >反馈</NavLink>
                 </li>
             </ul>
         </header>
